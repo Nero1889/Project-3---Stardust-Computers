7c772b9e8ff23f9ee5388e0e9fa7ccccb2f5a623
Raul Luna <mceclipse1889@gmail.com>
2025-02-27 22:18:13 -0600

    Update main.js

--- js-docs/main.js
+++ js-docs/main.js
@@ -44,30 +44,3 @@ THEME_TOGGLE_BTNS.forEach(btn => {
         document.body.style.transition = "background-color .2s ease-in-out";
     });
 }); 
-
-/* Search Engine (Prototype) */
-const SEARCH_INPUT = document.querySelector(".search-engine");
-const STORE_CONTAINERS = document.querySelectorAll(".store-container");
-
-SEARCH_INPUT.addEventListener("input", () => {
-    const SEARCH_TERM = SEARCH_INPUT.value.toLowerCase();
-
-    STORE_CONTAINERS.forEach(container => {
-        const ITEMS = container.querySelectorAll(".item");
-        const PARTS = container.querySelectorAll(".parts");
-
-        ITEMS.forEach(item => {
-            const KEYWORDS = item.dataset.keywords.toLowerCase();
-            KEYWORDS.includes(SEARCH_TERM)
-            ? item.classList.remove("hidden")
-            : item.classList.add("hidden");
-        });
-
-        PARTS.forEach(part => {
-            const KEYWORDS = part.dataset.keywords.toLowerCase();
-            KEYWORDS.includes(SEARCH_TERM)
-            ? part.classList.remove("hidden")
-            : part.classList.add("hidden");
-        });
-    });
-});
